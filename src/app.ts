0a6c43c02b991621ef4e4741b1563daddc9d743d
iZenrix <fajrulf960@gmail.com>
2024-11-05 11:55:48 +0700

    [MINOR-UPDATE] refactor auth api route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import userRoutes from "./routes/UserRoutes";
+import authRoutes from "./routes/AuthRoutes";
 
 const app = express();
 app.use(express.json());
 
 app.use('/users', userRoutes);
+app.use('/', authRoutes);
 
 export default app;
