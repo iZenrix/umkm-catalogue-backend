4055e448d592812b1d117b48227fdfed163ffecb
iZenrix <fajrulf960@gmail.com>
2024-11-09 19:56:48 +0700

    [MINOR-UPDATE] cors config

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import app from './app';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server is running`);
 });
 
