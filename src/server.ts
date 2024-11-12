39c9c2088d17dcc1ad6611f570e9717195b87a00
iZenrix <fajrulf960@gmail.com>
2024-11-13 03:29:48 +0700

    [UPDATE] vercel deploy config

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import app from './app';
 import { config } from './config/env';
 
-const port = config.port || 3000;
-
-app.listen(port, () => {
-    console.log(`Server running in ${config.nodeEnv} mode on port ${config.port}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(config.port, () => {
+        console.log(`Server running in ${config.nodeEnv} mode on port ${config.port}`);
+    });
+}
 
 export default app;
