cb89decf7769f469ea0b20476c27339a6033c218
iZenrix <fajrulf960@gmail.com>
2024-11-12 15:01:18 +0700

    [UPDATE] vercel config

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
 import app from './app';
 import { config } from './config/env';
-
+module.exports = app;
 app.listen(config.port, () => {
     console.log(`Server running in ${config.nodeEnv} mode on port ${config.port}`);
 });
 
-module.exports = app;
