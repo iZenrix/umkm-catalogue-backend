62404b32ba80cd03f37c6b4f54b678644ce5c690
iZenrix <fajrulf960@gmail.com>
2024-11-14 07:42:24 +0700

    [MINOR-UPDATE] rename file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import app from './app';
 import { config } from './config/env';
+
 app.listen(config.port, () => {
     console.log(`Server running in ${config.nodeEnv} mode on port ${config.port}`);
 });
-
-export default app;
 
 module.exports = app;
