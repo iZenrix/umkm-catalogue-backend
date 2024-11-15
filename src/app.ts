e036ca07ec56f2bcf4f9ff3c8c93813602907a1c
iZenrix <fajrulf960@gmail.com>
2024-11-15 22:19:36 +0700

    [FIX] cors option

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import apiRouter from "./api";
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://umkm-catalogue-frontend.vercel.app/'],
     credentials: true,
     optionsSuccessStatus: 200,
     allowedHeaders: ['Content-Type', 'Authorization'],
